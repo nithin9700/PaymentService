692c744221c0cc66673eaaf64b985007ea888616
nithin9700 <nithinkorukonda132@gmail.com>
2024-06-04 23:27:41 +0530

    Added Server Directory and Client

--- src/main/java/com/nithin/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/nithin/paymentservice/PaymentServiceApplication.java
@@ -1,6 +1,8 @@
 package com.nithin.paymentservice;
 
-import jdk.jfr.Enabled;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,8 +12,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class PaymentServiceApplication {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+    @Autowired
+    EurekaClient eurekaClient;
+
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
 
+    public void addApplication(){
+        eurekaClient.getApplications(applicationName);
+    }
 }
